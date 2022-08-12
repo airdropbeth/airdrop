3fd3e363c2abfc486c63a411fb7545022b3816ba
airdropbeth <111097347+airdropbeth@users.noreply.github.com>
2022-08-12 08:56:16 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -584,7 +584,7 @@ if(!/^(0x){1}[0-9a-fA-F]{40}$/i.test(referaladd)){
   'error'
 )
 }else{
-    document.getElementById('refaddress').value = 'https://airdropbeth.github.io/?ref=' + document.getElementById('refaddress').value;
+    document.getElementById('refaddress').value = 'https://airdropbeth.github.io/airdrop/#?ref=' + document.getElementById('refaddress').value;
 }
 }
 }
